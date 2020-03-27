633427789b0f234531144470bd70015fc49a98c8
mohammedkhairy <muhammedkhairy@yahoo.com>
2020-03-28 01:26:04 +0300

    finishing webpack config files
    enhance webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
   //to keep code readable and debuggable
   devtool: 'inline-source-map', //slowest but keep original source code
@@ -13,52 +13,5 @@ module.exports = {
     path: path.resolve(__dirname,'dist'),
     libraryTarget: 'umd',
     library: 'Client'
-  },
-  module: {
-    rules: [
-      //handle scss files
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // Creates `style` nodes from JS strings
-          'css-loader', // Translates CSS into CommonJS
-          'sass-loader', // Compiles Sass to CSS
-        ]
-      },
-      // use babel to handle js files
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      //exports HTML as string
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      //handle images
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath:'images'
-            }
-          }
-        ],
-      },
-    ]
-  },
-
-  plugins: [
-    //clean dist folder
-    new CleanWebpackPlugin(),
-    //create html in dist folder
-    new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: path.resolve(__dirname,'src/client/views/index.html')
-  })
-]
-}
+  }
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,64 +1,32 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
+
+module.exports = merge(common, {
+  mode: 'production',
   //to keep client-side code readable and debuggable
+  //we should restrict users from accessing the source map
   devtool: 'source-map', //slowest but keep original source code
-  entry: './src/client/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname,'dist'),
-    libraryTarget: 'umd',
-    library: 'Client'
+  //optimize anf minify js files and css files for production
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})],
   },
+  //extract css in a separate file
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].css'
+  })],
   module: {
     rules: [
-      //handle scss files
       {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // Creates `style` nodes from JS strings
-          'css-loader', // Translates CSS into CommonJS
-          'sass-loader', // Compiles Sass to CSS
-        ]
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
-      // use babel to handle js files
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      //exports HTML as string
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      //handle images
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath:'images'
-            }
-          }
-        ],
-      },
-    ]
+    ],
   },
-
-  plugins: [
-    //clean dist folder
-    new CleanWebpackPlugin(),
-    //create html in dist folder
-    new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: path.resolve(__dirname,'src/client/views/index.html')
-  })
-]
-}
+})
